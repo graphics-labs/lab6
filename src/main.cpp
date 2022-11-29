466d9e891f1cee4945189efa5e110e7b9438039b
danilka108 <danilka108@pop-os.localdomain>
2022-11-29 15:11:03 +0700

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,229 +1,128 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include <math.h>
-#include <string.h>
 
-#define WINDOW_HEIGHT 800
-#define WINDOW_WIDTH 2 * WINDOW_HEIGHT
+#define WINDOW_WIDTH = 800
+#define WINDOW_HEIGHT = 800
 
-double RADIUS = 10;
-double YZ_ANGLE = M_PI / 4;
-double ZX_ANGLE = M_PI / 6;
+//GLfloat mat_ambient[] = { 0.0, 0.1, 0.06};
+//GLfloat mat_diffuse[] = { 0.0, 0.50980392, 0.50980392};
+//GLfloat mat_specular[] = { 0.50196078,	0.50196078,	0.50196078};
+//GLfloat mat_shininess[] = { 0.25 * 128.0 };
 
-GLfloat VERTICES[][3] = {
-    {-1.0, -1.0, -1.0}, {1.0, -1.0, -1.0}, {1.0, 1.0, -1.0}, {-1.0, 1.0, -1.0}, {-1.0, -1.0, 1.0}, {1.0, -1.0, 1.0}, {1.0, 1.0, 1.0}, {-1.0, 1.0, 1.0}};
 
-GLfloat COLORS[][3] = {{0.78, 0.12, 0.0}, {0.5, 0.3, 0.2}, {1.0, 0.23, 0.3}, {0.4, 0.3, 0.7}, {0.79, 0.4, 1.0}, {0.0, 0.5, 0.9}};
+GLfloat mat_ambient[] = { 0.2125, 0.1275, 0.054 };
+GLfloat mat_diffuse[] = { 0.714, 0.4284, 0.18144 };
+GLfloat mat_specular[] = { 0.393548, 0.271906, 0.166721 };
+GLfloat mat_shininess[] = { 0.2 * 128.0 };
 
-void init();
-void resize(int, int);
-void display();
-void resize(int, int);
+GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
 
-void Task1();
-void Task2();
 
-double getXEye();
-double getYEye();
-double getZEye();
 
-void setCameraPosition();
-void OnKeyboard(int, int, int);
+void init(void);
+void display(void);
+void reshape(int w, int h);
+void keyboard(unsigned char key, int x, int y);
+void enable_background_light();
+void enable_pointer_light_source();
+void enable_remote_light_source();
+void enable_projector();
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowPosition(50, 10);
-  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-  glutCreateWindow("Lab4");
-
-  glutSpecialFunc(OnKeyboard);
-
-  glutDisplayFunc(display);
-  glutIdleFunc(display);
-  glutReshapeFunc(resize);
-
-  init();
-  glutMainLoop();
-  return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
 }
 
-void init()
+void init(void)
 {
-  glEnable(GL_COLOR_MATERIAL);
-  glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0);
-  glEnable(GL_DEPTH_TEST);
-  glShadeModel(GL_FLAT);
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_SMOOTH);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
+    // enable_background_light();
+    // enable_pointer_light_source();
+    enable_remote_light_source();
+    // enable_projector();
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
+
 }
 
-void resize(int width, int height) {}
-
-void setCameraPosition()
+void enable_background_light()
 {
-  double a = cos(YZ_ANGLE);
-  gluLookAt(getXEye(), getYEye(), getZEye(), 0, 0, 0, 0,
-            YZ_ANGLE < 0 || YZ_ANGLE > M_PI ? -1 : 1, 0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    GLfloat global_ambient[] = { 0.1, 0.1, 0.1, 1.0 };
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
 }
 
-double getXEye() { return RADIUS * sin(YZ_ANGLE) * sin(ZX_ANGLE); }
-
-double getYEye() { return RADIUS * cos(YZ_ANGLE); }
-
-double getZEye() { return RADIUS * sin(YZ_ANGLE) * cos(ZX_ANGLE); }
-
-void OnKeyboard(int key, int x, int y)
+void enable_pointer_light_source()
 {
-  if (key == GLUT_KEY_UP)
-  {
-    YZ_ANGLE -= M_PI / 10;
-  }
-
-  if (key == GLUT_KEY_DOWN)
-  {
-    YZ_ANGLE += M_PI / 10;
-  }
-
-  if (key == GLUT_KEY_LEFT)
-  {
-    ZX_ANGLE -= M_PI / 10;
-  }
-
-  if (key == GLUT_KEY_RIGHT)
-  {
-    ZX_ANGLE += M_PI / 10;
-  }
-
-  if (YZ_ANGLE > M_PI)
-  {
-    YZ_ANGLE = -M_PI;
-  }
-
-  if (YZ_ANGLE < -M_PI)
-  {
-    YZ_ANGLE = M_PI;
-  }
-
-  if (YZ_ANGLE == 0)
-  {
-    YZ_ANGLE -= 0.001;
-  }
-
-  if (ZX_ANGLE > M_PI * 2)
-  {
-    ZX_ANGLE -= M_PI * 2;
-  }
-
-  if (ZX_ANGLE < -M_PI * 2)
-  {
-    ZX_ANGLE *= -1;
-  }
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    GLfloat light_position[] = { 1.0, 1.0, 1.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 }
 
-void polygon(int a, int b, int c, int d, int color)
+void enable_remote_light_source()
 {
-  glColor3fv(COLORS[color]);
-
-  glBegin(GL_POLYGON);
-
-  glVertex3fv(VERTICES[a]);
-
-  glVertex3fv(VERTICES[b]);
-
-  glVertex3fv(VERTICES[c]);
-
-  glVertex3fv(VERTICES[d]);
-
-  glEnd();
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 }
 
-void colorcube()
+void enable_projector()
 {
-  polygon(0, 3, 2, 1, 0);
-  polygon(2, 3, 7, 6, 1);
-  polygon(0, 4, 7, 3, 2);
-  polygon(1, 2, 6, 5, 3);
-  polygon(4, 5, 6, 7, 4);
-  polygon(0, 1, 5, 4, 5);
+    glLightf(GL_SPOT_DIRECTION, GL_SPOT_EXPONENT, GL_SPOT_CUTOFF);
 }
 
-void Task1()
+void display(void)
 {
-  // левый верхний
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glViewport(0, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-3, 3, -3, 3, 2, 20);
-  gluLookAt(RADIUS, 0, 0, 0, 0, 0, 0, 1, 0);
-  glMatrixMode(GL_MODELVIEW);
-  colorcube();
+    glColor3f(0.8, 0.5, 0.2);
 
-  //
-
-  // правый верхний
-
-  glViewport(WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 4,
-             WINDOW_HEIGHT / 2);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-3, 3, -3, 3, 2, 20);
-  gluLookAt(RADIUS, RADIUS, RADIUS, 0, 0, 0, 0, 1, 0);
-  glMatrixMode(GL_MODELVIEW);
-  colorcube();
-
-  // левый нижний
-
-  glViewport(0, 0, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-3, 3, -3, 3, 2, 20);
-  gluLookAt(0, 0, RADIUS, 0, 0, 0, 0, 1, 0);
-  glRotated(20.705, 1, 0, 0);
-  glRotated(22.208, 0, 1, 0);
-  // glRotated(-45, 1, 0, 0);
-  // glRotated(35.3, 0, 1, 0);
-
-  glMatrixMode(GL_MODELVIEW);
-
-  colorcube();
-
-  // правый нижний
-
-  glViewport(WINDOW_WIDTH / 4, 0, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glFrustum(-1, 1, -1, 1, 6, 35);
-
-  gluLookAt(RADIUS, RADIUS, RADIUS, 0, 0, 0, 0, 1, 0);
-  gluPerspective(0, 0, 1, 2);
-  glMatrixMode(GL_MODELVIEW);
-  colorcube();
+    glutSolidSphere(1, 100, 100);
+    glFlush();
 }
 
-void Task2()
+void reshape(int w, int h)
 {
-  glViewport(WINDOW_WIDTH / 2, 0, WINDOW_WIDTH / 2, WINDOW_HEIGHT);
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glFrustum(-1, 1, -1, 1, 2, 20);
-  setCameraPosition();
-  glMatrixMode(GL_MODELVIEW);
-
-  colorcube();
-
-  glutSwapBuffers();
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (w <= h)
+        glOrtho(-1.5, 1.5, -1.5 * (GLfloat)h / (GLfloat)w,
+            1.5 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
+    else
+        glOrtho(-1.5 * (GLfloat)w / (GLfloat)h,
+            1.5 * (GLfloat)w / (GLfloat)h, -1.5, 1.5, -10.0, 10.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
-void display()
+void keyboard(unsigned char key, int x, int y)
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  Task1();
-  Task2();
+    switch (key) {
+    case 27:
+        exit(0);
+        break;
+    }
 }
